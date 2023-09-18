f8721e07f86315a4b69f8c650943ede85b9e32ab
Bladimir J.  Aponte <40173234+blad1mir@users.noreply.github.com>
2023-09-18 07:53:40 -0400

    base project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecordsComponent } from './records/records.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:LoginComponent
+  },
+  {
+    path:'login',component: LoginComponent
+  },
+  {
+    path:'records',component: RecordsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecordsComponent } from './records/records.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordsComponent,
+    LoginComponent,
+    NavbarMenuComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
