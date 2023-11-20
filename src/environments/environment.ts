fb1ceae2598cb34b396235584933f29a760f95a6
Bladimir Aponte <bladimirapon@gmail.com>
2023-11-19 20:45:20 -0400

    testconection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://v.claimcenter.com:8000/api',
-  apiUrl: 'http://0.0.0.0:8000',
+  apiUrl: 'https://v.claimcenter.com:8000/api/',
+  //apiUrl: 'http://0.0.0.0:8000',
 };
 
 /*
