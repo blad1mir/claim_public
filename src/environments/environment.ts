ed1607e3401cb6af4453fbff1e7c105c81e9689a
Bladimir Aponte <bladimirapon@gmail.com>
2023-11-22 18:25:50 -0400

    local changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://v.claimcenter.com:8000/api/',
+  apiUrl: 'https://v.claimcenter.com:8000/api',
   //apiUrl: 'http://0.0.0.0:8000',
 };
 
